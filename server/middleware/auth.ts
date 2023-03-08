884d35e56b5aa0716f2f21f5122e47e22dc1aede
Andres Meidla <andres.meidla@gmail.com>
2023-03-08 13:02:59 +0200

    reverted middleware change

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -3,39 +3,57 @@ import jwt from 'jsonwebtoken';
 const NO_AUTH_ROUTE_REGEXES = [
   // allow /login with query parameters
   /\/login.*/g,
-  /\/api\/login.*/g,
+  '/api/login',
+  'api/login',
   /\/_ipx\/.*/g,
   /\/__nuxt_error\?.*/g,
 ];
 
 export default defineEventHandler((event) => {
-  const parsedUrl = new URL(event.node.req.url ?? '/', useRuntimeConfig().webAddress || 'http://localhost');
-  const pathname = parsedUrl.pathname; // parse the query string out
+  const parsedUrl = new URL(event.node.req.url ?? '/', useRuntimeConfig().webAddress);
+  const pathname = parsedUrl.pathname;
+  if (pathname.startsWith('/login')) {
+    return;
+  }
+  if (pathname.startsWith('login')) {
+    return;
+  }
+  if (event.node.req.url?.startsWith('/login')) {
+    return;
+  }
+  if (event.node.req.url?.startsWith('login')) {
+    return;
+  }
+  if (!pathname.startsWith('/api')) {
+    return;
+  }
+  const allowedRoute = NO_AUTH_ROUTE_REGEXES.find((r) => {
+    if (r instanceof RegExp) {
+      return new RegExp(r).test(pathname);
+    } else if (r === pathname) {
+      return true;
+    }
+    return false;
+  });
+  if (allowedRoute) {
+    return;
+  }
   const authCookie = getCookie(event, 'keskusToken');
   if (authCookie) {
     try {
-      const decoded = jwt.verify(authCookie, useRuntimeConfig().jwtSecret || '#1241250+u9123i3fghu9');
+      const decoded = jwt.verify(authCookie, useRuntimeConfig().jwtSecret);
       if (decoded) {
-        console.log('Auth Success', pathname);
         event.context.auth = decoded;
         return;
       }
     } catch (e) {
       console.warn('While verifying token:', e);
-      deleteCookie(event, 'keskusToken');
     }
   }
-  const allowedRoute = NO_AUTH_ROUTE_REGEXES.find((r) => new RegExp(r).test(pathname));
-  if (allowedRoute) {
-    console.log('Allow', pathname);
-    // no auth needed
-    return;
-  }
   if (pathname.startsWith('/api')) {
-    console.log('API Unauthorized', pathname);
     throw createError({ statusCode: 401, message: 'Unauthorized' });
   }
-  console.log('Redirect to login', event.node.req.url);
+  deleteCookie(event, 'keskusToken');
   // redirecting
   return sendRedirect(event, `/login?${new URLSearchParams({ initial: pathname, fullUrl: event.node.req.url || 'none' })}`, 307);
 });
